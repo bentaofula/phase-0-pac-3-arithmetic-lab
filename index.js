05e13e5805ccc7c233508321ad1d74f5f4b88319
Benta <bentaakitwi500@gmail.com>
2023-03-12 21:15:22 +0300

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+    expect(add).toExist
+}
+function subtract(a,b){
+    expect(substract).toExist
+}
+function multiply(a,b){
+    expect(multiply).toExist
+}
+function divide(a,b){
+    expect(divide).toExist
+}
+function add(a,b){
+    return(a + b)
+}
+function subtract(a,b){
+    return(a - b)
+}
+function multiply(a,b){
+    return(a * b)
+}
+function divide(a,b){
+    return(a / b)
+}
+function increment(n){
+    return(n+1)
+}
+function decrement(n){
+    return (n-1)
+}
+function makeInt(n){
+return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+
+    return parseFloat(n)
+}
+
